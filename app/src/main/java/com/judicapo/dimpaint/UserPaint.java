c39f0daa4c738cfc3af866c39fcac8bf8145ca89
Juan Carlos Hernandez Ramos <jc.hernandez129@uniandes.edu.co>
2018-12-17 18:54:35 +0100

    Save multiple paths

--- app/src/main/java/com/judicapo/dimpaint/UserPaint.java
+++ app/src/main/java/com/judicapo/dimpaint/UserPaint.java
@@ -8,10 +8,10 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.SparseArray;
 import android.view.MotionEvent;
 import android.view.View;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class UserPaint extends View {
@@ -21,10 +21,8 @@ public class UserPaint extends View {
     public static final int DEFAULT_BG_COLOR = Color.WHITE;
     private Random rnd = new Random();
     private static final float TOUCH_TOLERANCE = 4;
-    private float mX, mY;
-    private Path mPath;
     private Paint mPaint;
-    private ArrayList<UserPath> paths = new ArrayList<>();
+    private SparseArray<UserPath> paths = new SparseArray<>();
     private int currentColor;
     private int backgroundColor = DEFAULT_BG_COLOR;
     private int strokeWidth;
@@ -72,7 +70,8 @@ public class UserPaint extends View {
         canvas.save();
         mCanvas.drawColor(backgroundColor);
 
-        for (UserPath fp : paths) {
+        for (int size = paths.size(), i = 0; i < size; i++) {
+            UserPath fp = paths.valueAt(i);
             mPaint.setColor(fp.color);
             mPaint.setStrokeWidth(fp.strokeWidth);
             mPaint.setMaskFilter(null);
@@ -85,49 +84,62 @@ public class UserPaint extends View {
         canvas.restore();
     }
 
-    private void touchStart(float x, float y) {
-        mPath = new Path();
-        UserPath fp = new UserPath(currentColor, strokeWidth, mPath);
-        paths.add(fp);
-
-        mPath.reset();
-        mPath.moveTo(x, y);
-        mX = x;
-        mY = y;
+    private void touchStart(int pointId,float x, float y) {
+        UserPath fp = new UserPath(currentColor, strokeWidth, x, y, new Path());
+        fp.path.reset();
+        fp.path.moveTo(x, y);
+        paths.put(pointId, fp);
     }
 
-    private void touchMove(float x, float y) {
-        float dx = Math.abs(x - mX);
-        float dy = Math.abs(y - mY);
+    private void touchMove(int pointerId, float x, float y) {
+        UserPath fp = paths.get(pointerId);
+        float dx = Math.abs(x - fp.x);
+        float dy = Math.abs(y - fp.y);
 
         if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
-            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
-            mX = x;
-            mY = y;
+            fp.path.quadTo(fp.x, fp.y, (x + fp.x) / 2, (y + fp.y) / 2);
+            fp.x = x;
+            fp.y = y;
         }
     }
 
-    private void touchUp() {
-        mPath.lineTo(mX, mY);
+    private void touchUp(int pointerId) {
+/*        UserPath fp = paths.get(pointerId);
+        fp.path.lineTo(fp.x, fp.y);*/
+        paths.remove(pointerId);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         this.currentColor = Color.argb(255, this.rnd.nextInt(256), this.rnd.nextInt(256), this.rnd.nextInt(256));
-        float x = event.getX();
-        float y = event.getY();
+        // get pointer index from the event object
+        int pointerIndex = event.getActionIndex();
 
-        switch(event.getAction()) {
-            case MotionEvent.ACTION_DOWN :
-                touchStart(x, y);
+        // get pointer ID
+        int pointerId = event.getPointerId(pointerIndex);
+
+        // get masked (not specific to a pointer) action
+        int maskedAction = event.getActionMasked();
+
+        float x = event.getX(pointerIndex);
+        float y = event.getY(pointerIndex);
+
+        switch(maskedAction) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_POINTER_DOWN:
+                touchStart(pointerId, x, y);
                 invalidate();
                 break;
             case MotionEvent.ACTION_MOVE :
-                touchMove(x, y);
+                for (int size = event.getPointerCount(), i = 0; i < size; i++) {
+                    touchMove(event.getPointerId(i), event.getX(i), event.getY(i));
+                }
                 invalidate();
                 break;
-            case MotionEvent.ACTION_UP :
-                touchUp();
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+            case MotionEvent.ACTION_CANCEL:
+                touchUp(pointerId);
                 invalidate();
                 break;
         }
--- app/src/main/java/com/judicapo/dimpaint/UserPath.java
+++ app/src/main/java/com/judicapo/dimpaint/UserPath.java
@@ -5,11 +5,14 @@ import android.graphics.Path;
 public class UserPath {
     public int color;
     public int strokeWidth;
+    public float x, y;
     public Path path;
 
-    public UserPath(int color, int strokeWidth, Path path) {
+    public UserPath(int color, int strokeWidth, float mX, float mY, Path path) {
         this.color = color;
         this.strokeWidth = strokeWidth;
+        this.x = mX;
+        this.y = mY;
         this.path = path;
     }
 }
